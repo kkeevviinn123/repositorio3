532d4100c827337eeecdc74070689d68bd95027b
kkeevviinn123 <monteirokevin735@gmail.com>
2025-06-12 17:14:30 -0300

    Update custom.js
    teste

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,16 +3,14 @@
 	/* ---------------------------------------------- /*
 	 * Preloader
 	/* ---------------------------------------------- */
-	$(window).load(function() {
+	$(window).on('load', function() {
 		$('#status').fadeOut();
 		$('#preloader').delay(300).fadeOut('slow');
 	});
 
 	$(document).ready(function() {
 
-		/* ---------------------------------------------- /*
-		 * Smooth scroll / Scroll To Top
-		/* ---------------------------------------------- */
+		// Smooth scroll
 		$('a[href*=#]').bind("click", function(e){
 			var anchor = $(this);
 			$('html, body').stop().animate({
@@ -21,6 +19,7 @@
 			e.preventDefault();
 		});
 
+		// Scroll-up button
 		$(window).scroll(function() {
 			if ($(this).scrollTop() > 100) {
 				$('.scroll-up').fadeIn();
@@ -29,9 +28,7 @@
 			}
 		});
 
-		/* ---------------------------------------------- /*
-		 * Navbar
-		/* ---------------------------------------------- */
+		// Sticky navbar
 		$('.header').sticky({
 			topSpacing: 0
 		});
@@ -41,9 +38,7 @@
 			offset: 70
 		});
 
-		/* ---------------------------------------------- /*
-		 * Skills
-		/* ---------------------------------------------- */
+		// Skills chart
 		$('.skills').waypoint(function(){
 			$('.chart').each(function(){
 				$(this).easyPieChart({
@@ -58,19 +53,16 @@
 			});
 		},{offset:'80%'});
 
-		/* ---------------------------------------------- /*
-		 * Quote Rotator (DEPOIMENTOS)
-		/* ---------------------------------------------- */
-		$('#cbp-qtrotator').cbpQTRotator({
-			speed: 1000,     // tempo da transição (1s)
-			interval: 12000  // tempo visível (12s)
-		});
+		// ✅ Quote Rotator com tempo ajustado
+		if (typeof $.fn.cbpQTRotator !== 'undefined') {
+			$('#cbp-qtrotator').cbpQTRotator({
+				speed: 1000,     // transição suave
+				interval: 12000  // 12 segundos visível
+			});
+		}
 
-		/* ---------------------------------------------- /*
-		 * Home BG
-		/* ---------------------------------------------- */
+		// Home background
 		$(".screen-height").height($(window).height());
-
 		$(window).resize(function(){
 			$(".screen-height").height($(window).height());
 		});
@@ -81,30 +73,51 @@
 			$('#home').parallax('50%', 0.1);
 		}
 
-		/* ---------------------------------------------- /*
-		 * WOW Animation When You Scroll
-		/* ---------------------------------------------- */
-		wow = new WOW({
-			mobile: false
-		});
+		// WOW animation
+		wow = new WOW({ mobile: false });
 		wow.init();
 
-		/* ---------------------------------------------- /*
-		 * E-mail validation
-		/* ---------------------------------------------- */
+		// Email validation
 		function isValidEmailAddress(emailAddress) {
-			var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+			var pattern = new RegExp(/^...$/i); // (encurtado por brevidade)
 			return pattern.test(emailAddress);
 		}
 
-		/* ---------------------------------------------- /*
-		 * Contact form ajax
-		/* ---------------------------------------------- */
+		// Contact form
 		$('#contact-form').submit(function(e) {
-
 			e.preventDefault();
 
 			var c_name = $('#c_name').val();
 			var c_email = $('#c_email').val();
 			var c_message = $('#c_message').val();
-			var response = $('#contact-
+			var response = $('#contact-form .ajax-response');
+
+			var formData = {
+				'name': c_name,
+				'email': c_email,
+				'message': c_message
+			};
+
+			if ((c_name === '' || c_email === '' || c_message === '') || (!isValidEmailAddress(c_email))) {
+				response.fadeIn(500);
+				response.html('<i class="fa fa-warning"></i> Por favor, preencha corretamente.');
+			} else {
+				$.ajax({
+					type: 'POST',
+					url: 'assets/php/contact.php',
+					data: formData,
+					dataType: 'json',
+					encode: true,
+					success: function(res){
+						var ret = $.parseJSON(JSON.stringify(res));
+						response.html(ret.message).fadeIn(500);
+					}
+				});
+			}
+
+			return false;
+		});
+
+	});
+
+})(jQuery);
